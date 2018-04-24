353ec78d224e02b557c764cf0c07c90d3b98e9bb
Khai Quang Nguyen <kqn001@bucknell.edu>
2018-04-24 09:17:51 -0400

    authro

--- node-labapp/labapp-server.js
+++ node-labapp/labapp-server.js
@@ -1,25 +1,27 @@
-const express = require('express')
+/*jshint esversion: 6 */
 
+const express = require("express");
 // config holds all constant values
-const config = require('./labapp-config.json')
-
+const config = require("./labapp-config.json");
 // MAIN APP.
-const app = express()
+const app = express();
 
 // setup CORS
-app.options('*', require("./cors"))
-
+app.options("*", require("./cors"));
+// setup session for production and testing
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
 // enable sessions on all routes
 app.use(require("./sessions"));
-
 // middlewares
-app.use(require('body-parser').json())
-
+app.use(require("body-parser").json());
 // ------------_ ROUTES ------------
 // /auth/login and /auth/logout routes.
-app.use(require("./auth"))
-
-app.use(require("./api"))
+app.use(require("./auth"));
+app.use(require("./api"));
 
 app.listen(config.port, () =>
-  console.log('labapp listening on port ' + config.port + '!'))
+  console.log("labapp listening on port " + config.port + "!")
+);
